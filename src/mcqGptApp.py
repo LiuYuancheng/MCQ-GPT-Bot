b825d9258e628dd35dcdb7eecd7e4e801cdebaa8
LiuYuancheng <liu_yuan_cheng@hotmail.com>
2023-08-29 14:19:20 +0800

    Added the log update code.

--- src/mcqGptApp.py
+++ src/mcqGptApp.py
@@ -19,15 +19,17 @@
 
 import os
 import json
+import threading
+from random import randint
 
 from datetime import timedelta, datetime
-from flask import Flask, render_template, request, flash, url_for, redirect
+from flask import Flask, render_template, request, flash, url_for, redirect, session
 from werkzeug.utils import secure_filename
-
-
+from flask_socketio import SocketIO, emit # pip install Flask-SocketIO==5.3.5
 import mcqGptAppGlobal as gv
 
 TEST_MD = False # Test mode flag.
+async_mode = None
 
 #-----------------------------------------------------------------------------
 # Init the flask web app program.
@@ -39,25 +41,66 @@ def createApp():
     app.config['UPLOAD_FOLDER'] = gv.UPLOAD_FOLDER
     return app
 
+def checkFile(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in gv.ALLOWED_EXTENSIONS
+
+
+def background_thread():
+    """Example of how to send server generated events to clients."""
+    count = 0
+    while True:
+        socketio.sleep(3)
+        count += 1
+        price = randint(3,10)
+        socketio.emit('my_response',
+                      {'data': 'Bitcoin current price (USD): ' + str(price), 'count': count})
+
 #-----------------------------------------------------------------------------
 #-----------------------------------------------------------------------------
 app = createApp()
+socketio = SocketIO(app, async_mode=async_mode)
+thread = None
+threadLock = threading.Lock()
 
 #-----------------------------------------------------------------------------
 # web home request handling functions. 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', async_mode=socketio.async_mode)
+
+@socketio.event
+def my_event(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': message['data'], 'count': session['receive_count']})
+
+
+@socketio.on('test_message')
+def handle_message(data):
+    print('received message: ' + str(data))
+    emit('test_response', {'data': 'Test response sent'})
+
+
+# Broadcast a message to all clients
+@socketio.on('broadcast_message')
+def handle_broadcast(data):
+    print('received: ' + str(data))
+    emit('broadcast_response', {'data': 'Broadcast sent'}, broadcast=True)
+
+@socketio.event
+def connect():
+    global thread
+    with threadLock:
+        if thread is None:
+            thread = socketio.start_background_task(background_thread)
+    emit('my_response', {'data': 'Connected', 'count': 0})
+
 
 #-----------------------------------------------------------------------------
 @app.route('/introduction')
 def introduction():
-    return render_template('introduction.html')
-
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in gv.ALLOWED_EXTENSIONS
+    return render_template('introduction.html', posts=None)
 
 @app.route('/upload', methods = ['POST', 'GET'])  
 def upload():
@@ -67,7 +110,7 @@ def upload():
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
-        if file and allowed_file(file.filename):
+        if file and checkFile(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
     return render_template('index.html')
@@ -76,7 +119,6 @@ def upload():
 @app.route('/introduction')
 def schedulermgmt():
     return render_template('introduction.html')
-
 
 #-----------------------------------------------------------------------------
 @app.route('/<int:postID>')
